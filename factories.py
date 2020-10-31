e7695d1c147f10a00c0b0d7241a334a1d74820c9
Samuel Girardin <samuelgirardin@pm.me>
2020-10-31 19:29:53 +0100

    add basic files

---
+++ factories.py
@@ -0,0 +1,7 @@
+"""Factory for creating my_app model's"""
+# from core.faker import factory
+
+# from my_app.models import MyModel
+# class MyModelFactory(factory.django.DjangoModelFactory):
+#     class Meta:
+#         model = MyModel
